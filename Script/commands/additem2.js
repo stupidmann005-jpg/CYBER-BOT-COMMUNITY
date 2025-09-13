53dc1bf2e2aebb5b73c7843b5c5b584a3d355761
jisanshik005-beep <jisanshik005@gmail.com>
2025-09-13 12:00:33 +0600

    Update additem2.js

--- Script/commands/additem2.js
+++ Script/commands/additem2.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
-const { AuctionItems } = require("../../includes/database/models/auction");
+const { sequelize } = require("../../includes/database/index");
 
 module.exports = {
   config: {
